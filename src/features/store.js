3ed8bcb094c34d255c325f2149b306ddf50a2f10
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-11-17 16:14:44 +0500

    delete files

--- src/features/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import moviesReducer from "./movies/movieSlice";
-
-export const store = configureStore({
-  reducer: {
-    movies: moviesReducer,
-  },
-});
