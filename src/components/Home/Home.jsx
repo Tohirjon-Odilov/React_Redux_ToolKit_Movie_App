8b1e1fbb8d990b1a15b206f7528708fcc7b48c80
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-11-20 01:59:33 +0500

    update files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import movieApi from "../../common/apis/movieApi";
+import MovieListing from "../MovieListing/MovieListing";
+import { APIKey } from "../../common/apis/movieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  const movieText = "Harry";
+  const movieType = "movie";
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=${movieType}`)
+        .catch((err) => {
+          console.log("Err: ", err);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
